153059c831b4120c760c1d41e3caf001ab0f1c9e
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-10 20:42:36 -0500

    starting script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+class Player {}
+
+class Projectile {}
+
+class Enemy {}
+
+class Game {}
+
+window.addEventListener("load", function () {
+  const canvas = document.getElementById("canvas1");
+});
