bf6fe8b07ae32428134be26189213e1882ffa340
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-12 18:00:57 -0500

    working on keyboard controls

--- script.js
+++ script.js
@@ -5,14 +5,20 @@ class Player {
     this.height = 100;
     this.x = this.game.width * 0.5 - this.width * 0.5;
     this.y = this.game.height - this.height;
-    this.speed = 25;
+    this.speed = 10;
   }
   draw(context) {
     context.fillRect(this.x, this.y, this.width, this.height);
   }
   update() {
+    // horizontal movement
     if (this.game.keys.indexOf("ArrowLeft") > -1) this.x -= this.speed;
     if (this.game.keys.indexOf("ArrowRight") > -1) this.x += this.speed;
+
+    // horizontal boundaries
+    if (this.x < 0) this.x = 0;
+    else if (this.x > this.game.width - this.width)
+      this.x = this.game.width - this.width;
   }
 }
 
