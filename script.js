9b5029527841918c6c1efb73c5eea005ed546dcd
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-14 04:26:18 -0500

    working on wave class

--- script.js
+++ script.js
@@ -85,23 +85,28 @@ class Wave {
     this.speedX = 3;
     this.speedY = 0;
     this.enemies = [];
+    this.create();
   }
   render(context) {
     this.speedY = 0;
     context.strokeRect(this.x, this.y, this.width, this.height);
-    this.x += this.speedX;
     if (this.x < 0 || this.x > this.game.width - this.width) {
       this.speedX *= -1;
       this.speedY = this.game.enemySize;
-      this.x += this.speedX;
-      this.y += this.speedY;
     }
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.enemies.forEach((enemy) => {
+      enemy.update();
+      enemy.draw(context);
+    });
   }
   create() {
     for (let y = 0; y < this.game.rows; y++) {
-      for (let y = 0; x < this.game.columns; x++) {
+      for (let x = 0; x < this.game.columns; x++) {
         let enemyX = x * this.game.enemySize;
         let enemyY = y * this.game.enemySize;
+        this.enemies.push(new Enemy(this.game, enemyX, enemyY));
       }
     }
   }
