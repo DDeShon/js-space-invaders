58e7f19a05d38c85cefc2025c454016489752ea7
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-21 16:11:34 -0500

    working on additional enemies

--- script.js
+++ script.js
@@ -187,6 +187,18 @@ class Beetlemorph extends Enemy {
   }
 }
 
+class Rhinomorph extends Enemy {
+  constructor(game, positionX, positionY) {
+    super(game, positionX, positionY);
+    this.image = document.getElementById("rhinomorph");
+    this.frameX = 0;
+    this.frameY = Math.floor(Math.random() * 4);
+    this.maxFrame = 2;
+    this.lives = 1;
+    this.maxLives = this.lives;
+  }
+}
+
 class Wave {
   constructor(game) {
     this.game = game;
